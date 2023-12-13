997f34cd1f7445c60e9a00476a93acb1d25f8634
cptbbVM <you@example.com>
2023-12-13 17:57:35 +0100

    satus bar for dwm

---
+++ statusbar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+date=$(date +"%a %d %b. %Y")
+hour=$(date +"%H:%M")
+battery=$(upower -i $(upower -e | grep BAT) | grep percentage | sed "s/ //g" | cut -d ":" -f2)
+volume=$(amixer get Master | grep dB | head -n 1 | cut -f7 -d' ' | sed "s/\[\|\]//g")
+brightness=$(echo "100 * $(xrandr --prop --verbose | grep -A10 " connected" | grep Brightness | cut -f2 -d' ')" | bc | sed "s/\.[0-9]$/%/g")
+
+echo "statussed" >> /home/cptbb/tozi
+
+xsetroot -name "| lum: $brightness | vol: $volume | bat: $battery | $date | $hour |"
