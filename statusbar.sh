e6a66f9a7d8f382664e275b3525b53cb06188033
_N3m0 <n3m0o7@hotmail.com>
2023-12-14 18:08:00 +0100

    statusbar : reseaux

--- statusbar.sh
+++ statusbar.sh
@@ -2,13 +2,14 @@
 
 while true; do
 
+wifi=$(iwgetid -r)
 date=$(date +"%a %d %b. %Y")
 hour=$(date +"%H:%M")
 battery=$(upower -i $(upower -e | grep BAT) | grep percentage | sed "s/ //g" | cut -d ":" -f2)
 volume=$(amixer get Master | grep dB | head -n 1 | cut -f7 -d' ' | sed "s/\[\|\]//g")
 brightness=$(echo "100 * $(xrandr --prop --verbose | grep -A10 " connected" | grep Brightness | cut -f2 -d' ')" | bc | sed "s/\.[0-9]$/%/g")
 
-xsetroot -name "| lum: $brightness | vol: $volume | bat: $battery | $date | $hour |"
+xsetroot -name "| res: $wifi | lum: $brightness | vol: $volume | bat: $battery | $date | $hour |"
 
 sleep 15
 done
